4aa778464644f033ded417cbb6f57c4e721023d0
Pepseso <juna.josef@gmail.com>
2021-05-02 15:26:43 +0200

    building blocks

---
+++ script.js
@@ -0,0 +1,39 @@
+let variable = 12;
+let myVariable = true;
+if (myVariable === true) {
+  console.log('The condition has been evaluated to true');
+}
+let a = 7;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a * 2);
+}
+let bookCount = 10;
+
+for (let i = 0; i < bookCount; i++) {
+console.log (i*2);
+}
+let add = (a, b) => {
+    return a + b;  
+  };
+  
+  console.log(add(6, 20))
+  console.log("excercise1");
+let b = 15;
+  for (let a = 1; a <= b; a++) {
+   
+    console.log(a)
+}
+let getGreetingsTo = (c) => {
+    return "Hello "+ (c) + "!";  
+  };
+  console.log(getGreetingsTo('Mark'));
+
+let colors = ["red", "green", "blue", "black", "brown", "purple", "yellow"]  
+colors.forEach ((number) =>{
+ {
+    console.log (number)
+    }
+});
+
